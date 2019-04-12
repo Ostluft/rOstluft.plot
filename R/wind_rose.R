2f0943a4135b58c21d00d06d706bec406b2e52fe
Thomas von Allmen <42243088+ugzvot@users.noreply.github.com>
2019-04-12 18:37:29 +0200

    WIP: wind_density & wind_rose_map

---
+++ R/wind_rose.R
@@ -0,0 +1,70 @@
+#' Plotting a windrose on a stamen terrain plot
+#'
+#' @param data in rolf format with WD and WVv
+#' @param meta tibble with one row containing x and y coordinates of the site
+#'
+#' @return png image
+#' @export
+plt_wind_rose <- function(data, meta) {
+  wide <- rOstluft::rolf_to_openair(data)
+  winkel_sector <- 22.5
+  wide <- dplyr::mutate(
+    wide,
+    wd_shifted = .data$wd - winkel_sector / 2,
+    wd_class = ggplot2::cut_width(.data$wd_shifted, 22.5),
+    ws_class = forcats::fct_rev(ggplot2::cut_interval(.data$ws, length = 2))
+  )
+
+ wind_rose <- ggplot(wide, aes(x = wd_class, fill = ws_class, y = stat(count / sum(count)))) +
+    geom_bar(width=1, colour="grey80", size=0.5, alpha = 0.5, show.legend = TRUE) +
+    scale_x_discrete(drop = FALSE) +
+    scale_y_discrete(labels = function(x) {stringr::str_c(x*100, " %") }) +
+    scale_fill_viridis_d(direction = 1)
+
+ legend <- cowplot::get_legend(wind_rose)
+ legend <- cowplot::ggdraw() + cowplot::draw_grob(legend)
+
+ wind_rose <- wind_rose +
+    coord_polar(start = -((winkel_sector/2)/360) * 2*pi) +
+    theme_minimal() +
+    theme(
+      panel.background = element_rect(fill = "transparent",colour = NA),
+      plot.background = element_rect(fill = "transparent",colour = NA),
+      axis.text = ggplot2::element_blank(),
+      axis.title = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      legend.position= "none"
+    )
+
+  #
+  fig_wind_rose <- magick::image_graph(width = 600, height = 600, res = 72)
+  print(wind_rose)
+  dev.off()
+
+  # transparency doesnt work with print, do it now
+  fig_wind_rose <- magick::image_transparent(fig_wind_rose, "white", 0.01)
+
+
+  fig_legend <- magick::image_graph(width = 100, height = 800, res = 72, bg = "transparent")
+  print(legend)
+  dev.off()
+
+  bbox <- tibble::tibble(x = c(meta$x - 500, meta$x + 500), y = c(meta$y - 500, meta$y + 500))
+
+  bbox <- rOstluft::transform_projection(bbox, coord = c("x", "y"),
+                       initCRS = sp::CRS("+init=epsg:2056"),
+                       outCRS = sp::CRS("+init=epsg:4326"))
+
+  bbox <- c(left = bbox$x[1], right = bbox$x[2], bottom = bbox$y[1], top = bbox$y[2])
+
+  bg <- ggmap::get_stamenmap(bbox, zoom = 16, maptype = "terrain", source = "stamen") %>%
+    ggmap::ggmap(extent = "device", padding = 0)
+
+
+  fig_bg <- magick::image_graph(width = 800, height = 800, res = 72)
+  print(bg)
+  dev.off()
+
+  img <- magick::image_composite(fig_bg, fig_wind_rose, operator = "add", offset= "+100+100")
+  magick::image_composite(img, fig_legend)
+}
--- README.md
+++ README.md
@@ -21,6 +21,7 @@ Beispiele
 =========
 
 Hysplit Trajektorien:
+---------------------
 
 ``` r
 library(rOstluft.plot)
@@ -31,3 +32,33 @@ hysplit_traj(traj, color_scale = ggplot2::scale_color_viridis_c(name = "m agl.")
 ```
 
 <img src="man/figures/README-unnamed-chunk-2-1.png" width="100%" />
+
+Wind Density
+------------
+
+Inspiriert bei [visualising diurnal wind climatologies](https://www.r-bloggers.com/visualising-diurnal-wind-climatologies-2/)
+
+WIP
+
+``` r
+library(rOstluft)
+store <- storage_s3_rds("aqmet", format = format_rolf(), bucket = "rostluft", prefix = "aqmet")
+data <- store$get(site = "ETHZ_CHN-Gebäude", year = 2017, interval = "h1")
+plt_wind_density(data)
+```
+
+<img src="man/figures/README-unnamed-chunk-3-1.png" width="100%" />
+
+Wind Rose mit Background Karte
+------------------------------
+
+WIP
+
+``` r
+# get needed meta information, one row with x and y coordinates in lsv95
+ethz <- store$get_meta("ethz")[[1]]
+site <- ethz[1, ]
+plt_wind_rose(data, site)
+```
+
+<img src="man/figures/README-unnamed-chunk-4-1.png" width="100%" />
