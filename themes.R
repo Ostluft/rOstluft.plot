d62bd6b36702efd6518d935a7e563962d445fa75
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-05-04 10:38:57 +0200

    custom themes

---
+++ themes.R
@@ -0,0 +1,9 @@
+theme_radar <-
+  theme_minimal() +
+  theme(
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank()
+  )
