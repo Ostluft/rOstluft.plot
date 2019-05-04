35c1cc9e91efe1d390dec5ddb99bc2fdca490363
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-05-04 10:43:02 +0200

    ...

--- themes.R
+++ themes.R
@@ -1,9 +1,32 @@
 theme_radar <-
-  theme_minimal() +
-  theme(
-    panel.background = element_blank(),
-    panel.grid = element_blank(),
-    axis.title.x = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank()
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    panel.background = ggplot2::element_blank(),
+    panel.grid = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_blank(),
+    axis.text.x = ggplot2::element_blank(),
+    axis.ticks.x = ggplot2::element_blank()
   )
+
+
+
+theme_traj <-
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    axis.text = ggplot2::element_blank(),
+    axis.title = ggplot2::element_blank(),
+    axis.ticks = ggplot2::element_blank()
+  )
+
+
+
+theme_polarplot <- 
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    axis.ticks.y = ggplot2::element_line(),
+    panel.ontop = TRUE,
+    panel.grid.minor = ggplot2::element_blank(),
+    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80")
+  )
+
+
