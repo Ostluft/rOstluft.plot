af0413cf20628a677b304e14ad18b24f5140b4eb
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-05-06 20:30:06 +0200

    coord_radar

---
+++ R/coord_radar.R
@@ -0,0 +1,7 @@
+coord_radar <- function (theta = "x", start = 0, direction = 1) { # sneaked from here: http://www.cmap.polytechnique.fr/~lepennec/R/Radar/RadarAndParallelPlots.html
+  theta <- match.arg(theta, c("x", "y"))
+  r <- if (theta == "x") "y" else "x"
+  ggproto("CordRadar", CoordPolar, theta = theta, r = r, start = start, 
+          direction = sign(direction),
+          is_linear = function(coord) TRUE)
+}
