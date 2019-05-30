587c93b2284978d14fb7ea5c7cf47fb2f44a2b7d
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-05-30 14:04:40 +0200

    included doc

--- R/get_ggmap.R
+++ R/get_ggmap.R
@@ -1,13 +1,21 @@
+#' Wrapper to load a ggmap 
+#'
+#' @param coord a data.frame or tibble with coordinates (latlon) defining the map extensions () in the form of coord = tibble(lon = ..., lat = ..., type = c("center", "lb", "rt", ...)); 
+#' the reason for this structure is, that ggmap::get_googlemap requires a different way (center + zoom) to specify the map frame than ggmap::get_stamenmap (bounding box). If needed, coord is
+#' converted to bounding box by rOstluft.plot::bbox() which takes type == "lb" as bottom & left coordinate, and "rt" as top & right, respectively.
+#' @param googlekey yourt google api key as a character string (see ?ggmap::get_googlemap; then, a google map is downloaded) or, if NA, a map via  ggmap::get_stamenmap is returned
+#'
+#' @return ggplot / ggmap object
+#' @export
 get_ggmap <- function(coord, 
                       googlekey = NA, 
                       zoom = 12, 
-                      shape = 3, 
-                      color = "gray20", 
-                      scale = 0.8,
-                      midpoint = FALSE,
                       mapstyle = c(feature = "all", element = "labels", visibility = "off"),
                       maptype = "terrain",
                       mapcolor = "bw",
+                      midpoint = FALSE,
+                      shape = 3, 
+                      color = "gray20", 
                       ...) {
   if (is.na(googlekey)) {
     map <- ggmap::get_stamenmap(bbox = bbox(coord), ...)
@@ -19,8 +27,12 @@ get_ggmap <- function(coord,
   map <-
     map %>% 
     ggmap::ggmap() +
-    geom_point(data = coord, aes(x = lon, y = lat), shape = shape, color = color) +
     theme_void()
+  if (midpoint) {
+    map <- 
+      map + 
+      geom_point(data = coord, aes(x = lon, y = lat), shape = shape, color = color)
+  }
   
   return(map)
 }
