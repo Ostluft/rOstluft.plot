ad18471bc36a3fa2971e727643a811e5af3e0319
Jörg Sintermann <joerg.sintermann@bd.zh.ch>
2019-06-12 16:59:26 +0200

    new and adjusted themes

--- R/themes.R
+++ R/themes.R
@@ -11,10 +11,11 @@ theme_traj <-
 theme_polarplot <-
   ggplot2::theme_minimal() +
   ggplot2::theme(
-    # axis.ticks.y = ggplot2::element_line()
+    axis.ticks.y = ggplot2::element_line(),
     panel.ontop = TRUE,
-    panel.grid.minor = element_blank(),
-    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80"),
+    panel.grid.minor.y = ggplot2::element_line(linetype = 2, color = "gray80", size = 0.25),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80", size = 0.25),
     axis.text.x = element_blank(),
     axis.title.y = element_blank()
   )
@@ -24,10 +25,22 @@ theme_polarplot <-
 theme_radar <-
   ggplot2::theme_minimal() +
   ggplot2::theme(
-    # axis.ticks.y = ggplot2::element_line()
+    axis.ticks.y = ggplot2::element_line(),
     panel.ontop = FALSE,
     panel.grid.minor = element_blank(),
-    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80"),
+    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80", size = 0.25),
+    axis.title.x = element_blank()
+  )
+
+
+
+theme_windrose <-
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    axis.ticks.y = ggplot2::element_line(),
+    panel.ontop = FALSE,
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major = ggplot2::element_line(linetype = 2, color = "gray80", size = 0.25),
     axis.title.x = element_blank()
   )
 
